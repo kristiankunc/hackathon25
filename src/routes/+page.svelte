1372984b87b1e1558ddee9c9769a1c724c704644
Kuba Vejražka <jakvejr@gmail.com>
2025-11-20 15:40:45 +0100

    embed pixi in div element on homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,26 @@
-<script lang="ts"></script>
+<script lang="ts">
+	import ShipPreview from "$lib/components/pixijs/shipPreview.svelte";
+</script>
 
-<div class="flex flex-col h-screen">
+<div class="flex h-screen flex-col">
+	<!-- Render area -->
+	<div class="static z-0 h-screen w-screen" id="render-div">
+		<ShipPreview />
+	</div>
 
-    <!-- Render area -->
-    <div class="render-div flex-1 w-full bg-gray-700" id="render-div"></div>
+	<!-- Buttons at bottom -->
+	<div class="buttons static bottom-10 z-10 flex justify-center gap-4 bg-gray-800 py-8">
+		<a class="button w-1/4 rounded-lg bg-blue-600 py-4 text-center text-xl font-semibold text-white transition hover:bg-blue-700" href="quests/"
+			>Quests</a
+		>
 
-    <!-- Buttons at bottom -->
-    <div class="buttons flex justify-center gap-4 py-8 bg-gray-800">
-        <a class="button w-1/4 text-center text-xl font-semibold py-4 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition"
-            href="quests/">Quests</a>
+		<a class="button w-1/4 rounded-lg bg-green-600 py-4 text-center text-xl font-semibold text-white transition hover:bg-green-700" href="game/"
+			>1V1</a
+		>
 
-        <a class="button w-1/4 text-center text-xl font-semibold py-4 bg-green-600 text-white rounded-lg hover:bg-green-700 transition"
-            href="game/">1V1</a>
-
-        <a class="button w-1/4 text-center text-xl font-semibold py-4 bg-purple-600 text-white rounded-lg hover:bg-purple-700 transition"
-            href="upgrades/">Upgrades</a>
-    </div>
-
+		<a
+			class="button w-1/4 rounded-lg bg-purple-600 py-4 text-center text-xl font-semibold text-white transition hover:bg-purple-700"
+			href="upgrades/">Upgrades</a
+		>
+	</div>
 </div>
