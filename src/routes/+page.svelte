c9ab172f5a19f646b523149a799fa68d5f1ffae5
Kuba Vejražka <jakvejr@gmail.com>
2025-11-20 17:55:17 +0100

    color pallete, updated home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,29 @@
 <script lang="ts">
+	import Header from "$lib/components/Header.svelte";
 	import ShipPreview from "$lib/components/pixijs/shipPreview.svelte";
 </script>
 
 <div class="flex h-screen flex-col">
+	<Header />
 	<!-- Render area -->
-	<div class="static z-0 h-screen w-screen" id="render-div">
+	<div class="z-0 h-full w-screen" id="render-div">
 		<ShipPreview />
 	</div>
 
 	<!-- Buttons at bottom -->
-	<div class="buttons static bottom-10 z-10 flex justify-center gap-4 bg-gray-800 py-8">
-		<a class="button w-1/4 rounded-lg bg-blue-600 py-4 text-center text-xl font-semibold text-white transition hover:bg-blue-700" href="quests/"
-			>Quests</a
-		>
-
-		<a class="button w-1/4 rounded-lg bg-green-600 py-4 text-center text-xl font-semibold text-white transition hover:bg-green-700" href="game/"
-			>1V1</a
+	<div class="buttons bottom-0 z-10 flex justify-center gap-4 bg-background p-4">
+		<a
+			class="button w-1/4 rounded-sm bg-secondary py-4 text-center text-xl font-semibold text-text transition-colors hover:bg-secondary-800"
+			href="quests/">Quests</a
 		>
 
 		<a
-			class="button w-1/4 rounded-lg bg-purple-600 py-4 text-center text-xl font-semibold text-white transition hover:bg-purple-700"
+			class="button w-1/4 rounded-sm bg-primary py-4 text-center text-2xl font-semibold text-primary-900 transition-colors hover:bg-primary-400"
+			href="game/">1V1</a
+		>
+
+		<a
+			class="button w-1/4 rounded-sm bg-secondary py-4 text-center text-xl font-semibold text-text transition-colors hover:bg-secondary-800"
 			href="upgrades/">Upgrades</a
 		>
 	</div>
