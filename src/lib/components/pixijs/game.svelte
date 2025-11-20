f1e6c2fcdb3a8edf5bf32584613ff7cb05023c71
Jakub Hanslik <jakub@hanslikovi.com>
2025-11-20 17:18:52 +0100

    Game loop

---
+++ src/lib/components/pixijs/game.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+	import { Application, Assets, Container, Sprite, Ticker } from "pixi.js";
+
+    const initPixieApp = async () => {
+        const parent = <HTMLDivElement>document.getElementById("render-div");
+		const app = new Application();
+
+		await app.init({ background: "#1099bb", resizeTo: parent });
+		parent.appendChild(app.canvas);
+
+		const container = new Container();
+		app.stage.addChild(container);
+
+        return {app, parentContainer: container}
+    }
+
+    const gameUpdate = (time: Ticker) => {
+
+    }
+
+	onMount(async () => {
+		(async () => {
+			const { app, parentContainer } = await initPixieApp()
+
+			
+
+
+			// Main loop
+			app.ticker.add(gameUpdate);
+		})();
+	});
+</script>
--- src/lib/components/pixijs/shipPreview.svelte
+++ src/lib/components/pixijs/shipPreview.svelte
@@ -1,52 +1,30 @@
 <script lang="ts">
 	import { onMount } from "svelte";
-	// description: This example demonstrates how to use a Container to group and manipulate multiple sprites
 	import { Application, Assets, Container, Sprite } from "pixi.js";
 
 	onMount(async () => {
 		(async () => {
 			const parent = <HTMLDivElement>document.getElementById("render-div");
-			console.log(parent);
-
-			// Create a new application
 			const app = new Application();
 
-			// Initialize the application
 			await app.init({ background: "#1099bb", resizeTo: parent });
-
-			// Append the application canvas to the document body
 			parent.appendChild(app.canvas);
 
-			// Create and add a container to the stage
+			
 			const container = new Container();
 
 			app.stage.addChild(container);
 
-			// Load the bunny texture
-			const texture = await Assets.load("https://pixijs.com/assets/bunny.png");
+			const texture = await Assets.load("https://i.redd.it/yct20hubfk061.png");
 
-			// Create a 5x5 grid of bunnies in the container
-			for (let i = 0; i < 25; i++) {
-				const bunny = new Sprite(texture);
+      const spaceship = new Sprite(texture)
+      container.addChild(spaceship)
+      spaceship.x = app.screen.width / 2 - texture.width / 2
+      spaceship.y = app.screen.height / 2 - texture.height / 2
 
-				bunny.x = (i % 5) * 40;
-				bunny.y = Math.floor(i / 5) * 40;
-				container.addChild(bunny);
-			}
-
-			// Move the container to the center
-			container.x = app.screen.width / 2;
-			container.y = app.screen.height / 2;
-
-			// Center the bunny sprites in local container coordinates
-			container.pivot.x = container.width / 2;
-			container.pivot.y = container.height / 2;
-
-			// Listen for animate update
+			// Main loop
 			app.ticker.add((time) => {
-				// Continuously rotate the container!
-				// * use delta to create frame-independent transform *
-				container.rotation -= 0.01 * time.deltaTime;
+				
 			});
 		})();
 	});
