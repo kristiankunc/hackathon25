60fa6123824f3e701c94a56b857f7f56787c6756
Ilja Novák <71844458+ilya496@users.noreply.github.com>
2025-11-21 08:26:07 +0100

    Upgrade tree (#35)
    * temp changes
    
    * beatiful upgrade tree
    
    * fixed node label for depth 0
    
    * added icon for root node
    
    * fixed aspect-ratio in ship's slot
    
    * pokus o nakup upgradu
    
    * functional buying

--- src/routes/upgrades/+page.svelte
+++ src/routes/upgrades/+page.svelte
@@ -5,7 +5,7 @@
 	export let data;
 </script>
 
-<Header {data}/>
+<Header {data} />
 <main class="mx-auto max-w-7xl px-4 py-6">
 	<h1 class="mb-3 text-3xl font-bold text-text">Upgrade Page</h1>
 	<p class="mb-3 text-lg text-text">Welcome to the upgrade page. Here you can find options to enhance your experience.</p>
